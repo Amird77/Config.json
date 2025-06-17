1d20096b46a0ac03176224cdbd33f44d03b77d17
Amird77 <amird8442@gmail.com>
2025-06-17 14:36:43 +0330

    Create Server.js

---
+++ Server.js
@@ -0,0 +1,9 @@
+const { exec } = require('child_process');
+exec('v2ray -config=config.json', (error, stdout, stderr) => {
+  if (error) {
+    console.error(`Error: ${error.message}`);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
+  console.error(`stderr: ${stderr}`);
+});
